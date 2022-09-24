27e99d1fabb4b1b92514abebe6d379d76066bc3e
Ragz <raghavsarin12@gmail.com>
2022-09-24 23:56:39 +0530

    added api call as well as print functionality for recipes

--- src/recipe.py
+++ src/recipe.py
@@ -1,6 +1,28 @@
+import requests
+import json
+
+apikey = ''
 
 print('What Ingredients do you currently have?')
-response = input()
+ingredients = input()
+ingredients = ingredients.replace(' ', '+')
 
-print(response.replace(' ', '+'))
+recipe_results = requests.get(f'https://api.spoonacular.com/recipes/findByIngredients?apiKey={apikey}&ingredients={ingredients}&number=10')
 
+
+def print_results(a):
+    print(json.dumps(recipe_results.json()[a]['title'], indent=4))
+
+print_results(0)
+
+print_results(1)
+
+print_results(2)
+
+print_results(3)
+
+print_results(4)
+
+print_results(5)
+
+print_results(6)
